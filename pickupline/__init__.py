e229e47db74f6ff3308b8b377ff1d2f08084ab31
prdpx7 <pradeepchoudhary009@gmail.com>
2018-08-13 00:57:00 +0530

    python3 support only

--- pickupline/__init__.py
+++
@@ -1,1 +0,0 @@
-from pickupline import PickupLine
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name="pickupline",
-      version="2.0",
+      version="2.1",
       description="Get PickupLines on various categories from web",
       url="http://github.com/prdpx7/pickup-line",
       author="Pradeep Khileri",
       author_email="pradeepchoudhary009@gmail.com",
       license="MIT",
-      packages=["pickupline"],
-      scripts=["./bin/pickup-line"],
+      packages=find_packages(),
+      scripts=['bin/pickup-line'],
       keywords='cli pickupline humour geek',
       install_requires=[
           "pyperclip",
